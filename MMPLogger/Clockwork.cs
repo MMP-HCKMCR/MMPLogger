e9b2a6ef4ee58d597c9a8872b2ee416f40d3c657
Matthew Kelly <badgerati42@gmail.com>
2014-10-25 16:02:47 +0100

    Initial commit

---
+++ MMPLogger/Clockwork.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMPLogger
+{
+    public class Clockwork
+    {
+
+        #region Constants
+
+        public string ENDPOINT = string.Empty;
+        public string API_KEY = string.Empty;
+
+        #endregion
+
+
+        #region Properties
+
+        private static Lazy<Clockwork> _lazy = new Lazy<Clockwork>(() => new Clockwork());
+
+        public static Clockwork Instance
+        {
+            get
+            {
+                return _lazy.Value;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Send(string number, string message)
+        {
+            if (string.IsNullOrEmpty(ENDPOINT) || string.IsNullOrEmpty(API_KEY) || string.IsNullOrEmpty(number))
+            {
+                return;
+            }
+
+            try
+            {
+                var url = string.Format("{0}?key={1}&to={2}&content={3}", ENDPOINT, API_KEY, number, message);
+                var request = (HttpWebRequest)WebRequest.Create(url);
+
+                using (var response = request.GetResponse())
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        Console.WriteLine(reader.ReadToEnd());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ MMPLogger/Service1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace MMPLogger
+{
+    public partial class Service1 : ServiceBase
+    {
+
+        #region Constructor
+
+        public Service1() //string[] args)
+        {
+            InitializeComponent();
+
+            /*string eventSourceName = "MMPLogger";
+            string logName = "MMPLoggerLog";
+
+            if (args.Count() > 0)
+            {
+                eventSourceName = args[0];
+            }
+
+            if (args.Count() > 1)
+            {
+                logName = args[1];
+            }
+
+            EventLog eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists(eventSourceName))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(eventSourceName, logName);
+            }
+
+            eventLog1.Source = eventSourceName;
+            eventLog1.Log = logName;
+
+            components.Add(eventLog1);*/
+        }
+
+        #endregion
+
+        #region Startup Methods
+
+        protected override void OnStart(string[] args)
+        {
+            Debugger.Break();
+
+            Clockwork.Instance.ENDPOINT = ConfigurationManager.AppSettings["clockwork_url"];
+            Clockwork.Instance.API_KEY = ConfigurationManager.AppSettings["clockwork_key"];
+            SystemEvents.SessionSwitch += SystemEvents_SessionSwitch;
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        #endregion
+
+        #region Session Logic
+
+        void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            DoAction(e.Reason);
+        }
+
+        private void DoAction(SessionSwitchReason reason)
+        {
+            switch (reason)
+            {
+                case SessionSwitchReason.SessionLogon:
+                    Clockwork.Instance.Send("07858055210", "Greetings!");
+                    break;
+
+                case SessionSwitchReason.SessionLogoff:
+                    Clockwork.Instance.Send("07858055210", "Don't leeeeave meeee! :(");
+                    break;
+
+                case SessionSwitchReason.SessionLock:
+                    Clockwork.Instance.Send("07858055210", "Bye~");
+                    break;
+
+                case SessionSwitchReason.SessionUnlock:
+                    Clockwork.Instance.Send("07858055210", "Welcome back~");
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
